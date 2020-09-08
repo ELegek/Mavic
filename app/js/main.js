b9b9a02bab1f283893f544d5740e63a25e4a18d1
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-09-08 14:31:28 +0300

    Адаптив меню

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,11 @@ $(function(){
         $(this).parent().addClass('questions__item--active');
     });
 
+
+    $('#fullpage').fullpage({
+		autoScrolling:true,
+        scrollHorizontally: true,
+        sectionSelector: '.page-section',
+	});
   
 });
